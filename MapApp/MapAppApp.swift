de6b1ffd423d826eeff5e0105b40db2709a40efb
Zhiyuan <zhou.zhiyuan.stephen@hotmail.com>
2023-12-09 14:04:31 -0500

    Initial Commit

---
+++ MapApp/MapAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MapAppApp.swift
+//  MapApp
+//
+//  Created by Zhiyuan Zhou on 12/9/23.
+//
+
+import SwiftUI
+
+@main
+struct MapAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
